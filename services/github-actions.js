47bf913700ce6ceaaf77a13b9a2420c40a9dd4e4
sebastiano <piccionesebastiano40@gmail.com>
2023-07-07 18:10:51 +0200

    update

--- services/github-actions.js
+++ services/github-actions.js
@@ -1,44 +1,10 @@
 "use strict";
 const axios = require("axios");
-const createError = require("http-errors");
-
-const pluginPkg = require("../package.json");
-const utils = require("../utils");
-
-function populateWorkflowWithEnvPat(workflow) {
-  try {
-    const config = utils.getConfig();
-    const hasEnvPat = config.hasEnvPat;
-
-    if (!hasEnvPat) {
-      return workflow;
-    }
-
-    const wfConfigPat = config.pats[workflow.name];
-
-    if (wfConfigPat) {
-      return {
-        ...workflow,
-        pat: wfConfigPat,
-      };
-    }
-
-    strapi.log.debug(
-      `${pluginPkg.name}: hasEnvPat is set to true but no token was found with name ${workflow.name}`
-    );
-    return workflow;
-  } catch (e) {
-    strapi.log.debug(`${pluginPkg.name}: ${e}`);
-    throw createError(500, "Invalid config");
-  }
-}
 
 module.exports = {
-  populateWorkflowWithEnvPat,
-
   getWorkflows: async () => {
     const workflows = await strapi
-      .query("workflow", "github-actions")
+      .query("workflow", "github-actions-workflowid")
       .find({ _sort: "name" }, []);
 
     return workflows;
@@ -46,10 +12,10 @@ module.exports = {
 
   triggerWorkflow: async (id) => {
     const dbWorkflow = await strapi
-      .query("workflow", "github-actions")
+      .query("workflow", "github-actions-workflowid")
       .findOne({ id });
 
-    const workflow = populateWorkflowWithEnvPat(dbWorkflow);
+    const workflow = dbWorkflow;
     const res = await axios.post(`https://api.github.com/repos/${workflow.repo_owner}/${workflow.repo_name}/actions/workflows/${workflow.name}/dispatches`,
       {
         ref: "main",
@@ -62,10 +28,8 @@ module.exports = {
       }
     );
 
-    console.log(res)
-
     await strapi
-      .query("workflow", "github-actions")
+      .query("workflow", "github-actions-workflowid")
       .update({ id }, { started_at: Date.now() });
 
     return {};
